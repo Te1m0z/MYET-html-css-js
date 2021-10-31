ad9e530940326d29088ce990972910d116e69669
te1m0z <dmitriy.balamozhnov@gmail.com>
2021-10-31 23:39:18 +0300

    images

--- src/static/js/script1.js
+++ src/static/js/script1.js
@@ -5,8 +5,20 @@ $('.slider-track').slick({
     slidesToShow: 4,
     autoplay: true,
     autoplaySpeed: 2000,
-    prevArrow: false,
-    nextArrow: false,
+    pauseOnHover: true,
+    prevArrow: `<button class="prev-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+            <path d="M0 0h24v24H0V0z" fill="none"/>
+            <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12l4.58-4.59z"/>
+        </svg>
+    </button>`,
+    nextArrow: `<button class="next-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+            <path d="M0 0h24v24H0V0z" fill="none"/>
+            <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z"/>
+        </svg>
+    </button>`,
+    touchThreshold: 10,
     responsive: [
         {
             breakpoint: 1220,
