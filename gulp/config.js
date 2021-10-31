448791de5d381e5753b27a8b501cdae819472903
te1m0z <dmitriy.balamozhnov@gmail.com>
2021-10-31 18:14:19 +0300

    mod gulp config

--- gulp/config.js
+++ gulp/config.js
@@ -1,7 +1,9 @@
 module.exports =  [
-    './gulp/tasks/sass.js',
+    './gulp/tasks/styles.js',
     './gulp/tasks/scripts.js',
+    './gulp/tasks/pug.js',
     './gulp/tasks/img.js',
     './gulp/tasks/serve.js',
     './gulp/tasks/watch.js',
+    './gulp/tasks/copy.js'
 ];
